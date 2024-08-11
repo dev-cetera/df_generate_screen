da12cccb1d24aae615317d07047882bd7f290dd1
robmllze <86869208+robmllze@users.noreply.github.com>
2024-08-11 20:48:51 +1000

    Update

--- lib/src/generate_screen_access/generator.dart
+++ lib/src/generate_screen_access/generator.dart
@@ -69,10 +69,10 @@ Future<void> generateScreenAccess({
     final filePath = filePathResult.path;
 
     // Extract insights from the file.
-    // final classInsights = await extractClassInsightsFromDartFile(
-    //   analysisContextCollection,
-    //   filePath,
-    // );
+    final classInsights = await extractClassInsightsFromDartFile(
+      analysisContextCollection,
+      filePath,
+    );
 
     // if (classInsights.isNotEmpty) {
     //   // Converge what was gathered to generate the output.
