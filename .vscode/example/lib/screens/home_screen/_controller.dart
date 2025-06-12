7ed1bd25ab762047c52ab86a78088ca0080c4168
robmllze <86869208+robmllze@users.noreply.github.com>
2025-06-11 20:23:17 -0400

    update

--- .vscode/example/lib/screens/home_screen/_controller.dart
+++
@@ -1,5 +0,0 @@
-part of 'widget.dart';
-
-final class HomeScreenController extends THomeScreenController {
-  HomeScreenController(super.screen, super.state);
-}
--- .vscode/example/lib/screens/login_screen/_controller.dart
+++
@@ -1,5 +0,0 @@
-part of 'widget.dart';
-
-final class LoginScreenController extends TLoginScreenController {
-  LoginScreenController(super.screen, super.state);
-}
